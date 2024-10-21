9155fb81cef3192fc77e07556372acec7030c8fb
Yahyakamran <yahyakamran099@gmail.com>
2024-10-22 01:27:58 +0500

    configured netrw

--- nvim/init.lua
+++ nvim/init.lua
@@ -46,7 +46,9 @@ vim.opt.incsearch = true
 vim.opt.scrolloff = 8
 vim.opt.list = true
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = false
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
 vim.opt.cursorline = true
 vim.opt.swapfile = false
 -- key maps
@@ -69,22 +71,8 @@ vim.api.nvim_set_keymap("n", "<leader>fa", ":Files ~/projects/<CR>", { noremap =
 --vim-fugitive
 vim.keymap.set('n','<leader>gc' , vim.cmd.Git)
 -- Open files in same directory as current file
-local opts = { noremap = true, silent = true }
 --LSP CONFIG
 local lsp_zero = require('lsp-zero')
-lsp_zero.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
@@ -148,4 +136,11 @@ vim.cmd [[colorscheme accent]]
 vim.cmd [[ highlight HighlightedyankRegion ctermbg=237 guibg=#707070]]
 vim.cmd [[ highlight CurSearch ctermbg=237 guibg=#707070]]
 vim.cmd [[ highlight IncSearch ctermbg=237 guibg=#707070]]
+
+
+--netrw
+
+vim.g.netrw_banner = 0
+
+
 
